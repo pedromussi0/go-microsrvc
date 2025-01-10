247881265bc078651de36f6321948e4562dfd8a8
Pedro Mussi <phpmussi@gmail.com>
2025-01-10 20:47:36 -0300

    feat: logger model initial set up

--- logger-service/data/models.go
+++ logger-service/data/models.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -79,4 +80,71 @@ func (l *LogEntry) All() ([]*LogEntry, error) {
 	}
 
 	return entries, nil
+}
+
+func (l *LogEntry) GetOne(id string) (*LogEntry, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	collection := client.Database("logger").Collection("log_entries")
+
+	docID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		log.Println("Error converting id to object id: ", err)
+		return nil, err
+	}
+
+	var entry LogEntry
+	err = collection.FindOne(ctx, bson.M{"_id": docID}).Decode(&entry)
+	if err != nil {
+		log.Println("Error finding log entry: ", err)
+		return nil, err
+	}
+
+	return &entry, nil
+}
+
+func (l *LogEntry) DropCollection() error {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+	collection := client.Database("logger").Collection("log_entries")
+
+	err := collection.Drop(ctx)
+	if err != nil {
+		log.Println("Error dropping log entries collection: ", err)
+		return err
+	}
+
+	return nil
+}
+
+func (l *LogEntry) Update() (*mongo.UpdateResult, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	collection := client.Database("logger").Collection("log_entries")
+
+	docID, err := primitive.ObjectIDFromHex(l.id)
+	if err != nil {
+		log.Println("Error converting id to object id: ", err)
+		return err
+	}
+
+	result, err := collection.UpdateOne(
+		ctx,
+		bson.M{"_id": docID},
+		bson.D{
+			{"$set", bson.D{
+				{"name", l.Name},
+				{"data", l.Data},
+				{"updated_at", time.Now()},
+			}},
+		},
+	)
+	if err != nil {
+		log.Println("Error updating log entry: ", err)
+		return nil, err
+	}
+
+	return result, nil
 }
